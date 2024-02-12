034b32fadc2ad4bf8b2f6a84d1243eb7e6666074
SoumyaCode74 <soumyadeep.work01@gmail.com>
2024-02-12 07:35:10 +0530

    Develop I2C periph drivers for STM32F429xx MCU

---
+++ Peripherals/I2C_Test/stm32f429_i2c_driver/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c ../Inc/stm32f429xx_i2c_driver.h \
+ ../Inc/stm32f429xx_general.h ../Inc/stm32f429xx.h \
+ C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Inc/system_stm32f4xx.h
+../Inc/stm32f429xx_i2c_driver.h:
+../Inc/stm32f429xx_general.h:
+../Inc/stm32f429xx.h:
+C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/core_cm4.h:
+C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Temp_work/Personal/GitHub/STM32_MCU_Peripherals/Peripherals/I2C_Test/stm32f429_i2c_driver/Drivers/CMSIS/Include/mpu_armv7.h:
+../Inc/system_stm32f4xx.h:
---
+++ Peripherals/I2C_Test/stm32f429_i2c_driver/Inc/stm32f429xx_general.h
@@ -0,0 +1,20 @@
+/*
+ * stm32f429xx_general.h
+ *
+ *  Created on: Feb 11, 2024
+ *      Author: Soumyadeep Bose
+ */
+
+#ifndef STM32F429XX_GENERAL_H_
+#define STM32F429XX_GENERAL_H_
+
+#include <stdint.h>
+#include "stm32f429xx.h"
+
+typedef RCC_TypeDef RCC_RegAddr_t;
+typedef GPIO_TypeDef GPIO_RegAddr_t;
+typedef USART_TypeDef USART_RegAddr_t;
+typedef SPI_TypeDef SPI_RegAddr_t;
+typedef I2C_TypeDef I2C_RegAddr_t;
+
+#endif /* STM32F429XX_GENERAL_H_ */
